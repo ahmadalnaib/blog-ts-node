db0a8d211d78da1c544d0cb62ccac2041188c3ab
ahmedhabeeb73 <alnaib888@gmail.com>
2024-08-11 18:15:29 +0200

    add ts

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,26 +1,32 @@
 <script setup lang="ts">
-import {ref} from 'vue';
-const periods=['Today', 'This Week', 'This Month'];
+import { type } from 'os';
+import { ref } from 'vue';
+type Period = 'Today' | 'This Week' | 'This Month';
 
-const  selectedPeriod= ref('Today');
+const periods:Period[] = ['Today', 'This Week', 'This Month'];
 
-const selectPeriod=(period:string)=>{
-  console.log(period);
-  selectedPeriod.value=period;
+const selectedPeriod = ref<Period>('Today');
 
-}
-
+const selectPeriod = (period: Period) => {
+  selectedPeriod.value = period;
+};
 </script>
 
 <template>
   <nav>
-    <div class="panel-tabs bg-white shadow-xl text-center text-blue-500 p-5 flex justify-center gap-5">
-
-      <a v-for="period in periods" :key="period" @click="selectPeriod(period)" class="tab-link px-4 py-2 rounded transition-colors duration-300 hover:bg-blue-500 hover:text-white">{{ period }}</a>
-     
+    <div
+      class="panel-tabs bg-white shadow-xl text-center text-blue-500 p-5 flex justify-center gap-5"
+    >
+      <a
+        v-for="period in periods"
+        :key="period"
+        @click="selectPeriod(period)"
+        class="tab-link px-4 py-2 rounded transition-colors duration-300"
+        :class="{ 'bg-blue-500 text-white': selectedPeriod === period }"
+        >{{ period }}</a
+      >
     </div>
   </nav>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
