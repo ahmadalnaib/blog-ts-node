b3276b2f322ece87f589160ddcdf3796b3ed5581
ahmedhabeeb73 <alnaib888@gmail.com>
2024-08-11 14:51:38 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Timeline from './components/Timeline.vue';
+</script>
+
+<template>
+  <div class="section"></div>
+  <div class="container mt-20">
+    <Timeline />
+  </div>
+</template>
+
+<style scoped></style>
